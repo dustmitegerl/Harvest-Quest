4a279b90c6b8c45e450f91094c8ea4b96637cbcf
dustmitegerl <ida.kenna@protonmail.com>
2024-10-02 16:34:26 -0400

    created the main menu placeholder, started setting up the event system

---
+++ Assets/UI/EventSystemExtender.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemExtender : MonoBehaviour
+{
+    GameObject currentlyHovered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Highlight(){
+	
+    }
+}

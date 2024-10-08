1625416358653857b483df5099aab49597acc667
Infernape26 <imani.mobley17@gmail.com>
2024-10-08 17:27:07 -0400

    Dash
    Added the dash to the player's movement and move the script.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    [SerializeField] float dashSpeed = 10f;
+    [SerializeField] float dashDuration = 1f;
+    [SerializeField] float dashCooldown = 1f;
+    bool isDashing;
+    bool canDash = true;
+
+    void Start()
+    {
+        canDash = true;
+    }
+
+    // Update is called once per frame
+    void Update() //Input
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(movement.x * dashSpeed, movement.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+}

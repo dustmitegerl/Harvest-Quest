853685779b8377583b6b6cc8d2ab234d8639c85e
ida <ida.kenna@protonmail.com>
2024-10-22 21:35:24 -0400

    finished up the timekeeper -- designer can set the starting hr, etc

--- Assets/Scripts/GameTime.cs
+++ Assets/Scripts/GameTime.cs
@@ -6,26 +6,47 @@ using JetBrains.Annotations;
 
 public class GameTime : MonoBehaviour
 {
-    
+    /// <summary>
+    /// prefabs
+    /// </summary>
     public TextMeshProUGUI clockRead; // the clock prefab's display text
     public TextMeshProUGUI dayRead; // the day indicator's display text
+
     /// <summary>
-    /// clock's visual stuff stuff
+    /// game timekeeping
     /// </summary>
-    public string AM = "a.m.";
-    public string PM = "p.m.";
-    public int timeSpeedModulator = 1; // used to change speed of seconds
-    public int secsInMin = 60;
-    public int minsInHr = 60;
-    public int hrsInDay = 24;
-    public float[] gameTime;
-    public float secs;
-    public int mins;
-    public int hrs;
+    [SerializeField]
+    int startingHr;
     public int days = 1; // starting days index at 1 rather than 0
+    public int hrs;
+    public int mins;
+    public float secs;
+
+    /// <summary>
+    /// clock's visual stuff
+    /// </summary>
+    [SerializeField]
+    string AM = "a.m.";
+    [SerializeField]
+    string PM = "p.m.";
+
+    [SerializeField]
+    int timeSpeedModulator = 1; // used to change speed of seconds
+    [SerializeField]
+    int secsInMin = 60;
+    [SerializeField]
+    int minsInHr = 60;
+    [SerializeField]
+    int hrsInDay = 24;
+    
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    void Awake()
+    {
+        hrs = startingHr;
     }
 
     // Update is called once per frame
@@ -65,6 +86,7 @@ public class GameTime : MonoBehaviour
             days += 1;
         }
     }
+
     /// <summary>
     /// updates the clock and days calendar displays. 
     /// i intend to replace this part and move it to the actual clock and calendar prefabs, 
@@ -73,30 +95,49 @@ public class GameTime : MonoBehaviour
     /// </summary>
     void UpdateTimePrefabs()
     {
+        string currentPeriod;
         int adjHrs = 0; // adjusting for am vs pm
-        string dayPeriod = AM; // default is a.m.
+        string minsString; // for adding a 0 before single-digit minutes
 
-        if (dayPeriod == AM) // during A.M. hours
+        if (hrs >= 12) // 12 oclock
+        {
+            currentPeriod = PM; // strikes noon
+        }
+        else currentPeriod = AM; // between midnight and 12 is morning
+
+        if (currentPeriod == AM) // during A.M. hours
         {
             adjHrs = hrs; // the hours aren't adjusted
             
             if (adjHrs >= 12) // when the clock strikes 12
             {
-                dayPeriod = PM; // period becomes PM
+                currentPeriod = PM; // period becomes PM
             }
         }
-
-        else if (dayPeriod == "p.m.") // during P.M. hours
+        else if (currentPeriod == PM) // during P.M. hours
         {
             adjHrs = hrs - 12; // subtract 12 to get the adjusted time
 
             if (adjHrs >= 12)
             {
-                dayPeriod = AM; // period becomes AM
+                currentPeriod = AM; // period becomes AM
             }
         }
 
-        clockRead.SetText(adjHrs.ToString() + ":" + mins.ToString()); // set the clock to the time, adjusting the hours
+        // in case of 0:00, set to 12AM
+        if (hrs == 0)
+        {
+            adjHrs = 12;
+            currentPeriod = AM;
+        }
+
+        if (mins < 10) // if less than 10 minutes on the clock, 
+        {
+            minsString = "0" + mins.ToString();
+        }
+        else minsString = mins.ToString(); 
+
+        clockRead.SetText(adjHrs.ToString() + ":" + minsString + currentPeriod); // otherwise just adjust hours and print
         dayRead.SetText("day " + days.ToString()); // set the day in the calendar object
     }
 }

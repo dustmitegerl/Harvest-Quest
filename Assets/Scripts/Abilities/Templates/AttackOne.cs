367a50ce160e41bc6ad350ffab4676464f2872f3
dustmitegerl <ida.kenna@protonmail.com>
2024-11-20 13:12:27 -0500

    small edit to AttackOne template

--- Assets/Scripts/Abilities/Templates/AttackOne.cs
+++ Assets/Scripts/Abilities/Templates/AttackOne.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackOne : Attack
+public abstract class AttackOne : Attack
 {
     [SerializeField]
     GameObject selected;

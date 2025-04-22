71747e730a59b7166389fd1e1ba2a7ec7f1fd613
pumpkined <153791149+pumpkined@users.noreply.github.com>
2025-04-22 03:21:36 -0400

    Added FUnctioning Lighting for Time of Day
    Created DayNight cycle using URP
    
    Added minsElasped to GameData
    
    GlobalLight prefab must be used for illumination
    
    all assets need SpriteLit material to respond to lighting

--- Assets/Scripts/System/GameData.cs
+++ Assets/Scripts/System/GameData.cs
@@ -7,5 +7,6 @@ public static class GameData
     public static int days = 1; // starting days index at 1 rather than 0
     public static int hrs;
     public static int mins;
+    public static float minsElapsed;
     public static float secs;
 }

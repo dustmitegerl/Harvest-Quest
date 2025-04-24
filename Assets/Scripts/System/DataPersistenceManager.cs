cbd9b956eda1159e45d7ef1514d9fd52071ad5e3
dagray0524 <devone.gray@ubalt.edu>
2025-04-23 20:01:36 -0500

    Update - 04/23/2025
    Added transition into Harvest Quest.
    Added SFX in the Sound (Volume) slider as suggested.
    Created Prefabs for SFX and Music.

--- Assets/Scripts/System/DataPersistenceManager.cs
+++ Assets/Scripts/System/DataPersistenceManager.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataPersistenceManager : MonoBehaviour
+<<<<<<< Updated upstream
 {
+=======
+{ //Reference: https://m.youtube.com/watch?v=aUi9aijvpgs&pp=0gcJCdgAo7VqN5tD
+
+    private GameData gameData;
+>>>>>>> Stashed changes
     public static DataPersistenceManager instance { get; private set; }
 
     private void Awake()
--- Assets/Scripts/System/GameData.cs
+++ Assets/Scripts/System/GameData.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameData
+[System.Serializable]
+public class GameData
 {
     public static int days = 1; // starting days index at 1 rather than 0
     public static int hrs;

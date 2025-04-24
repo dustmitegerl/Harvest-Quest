9a9db74c884b06351ca3547df67befa9e2d69680
dagray0524 <devone.gray@ubalt.edu>
2025-04-23 20:12:41 -0500

    update
    test

--- Assets/Scripts/System/DataPersistenceManager.cs
+++ Assets/Scripts/System/DataPersistenceManager.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataPersistenceManager : MonoBehaviour
-<<<<<<< Updated upstream
-{
-=======
+
+
 { //Reference: https://m.youtube.com/watch?v=aUi9aijvpgs&pp=0gcJCdgAo7VqN5tD
 
     private GameData gameData;
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+
+
     public static DataPersistenceManager instance { get; private set; }
 
     private void Awake()

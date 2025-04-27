e7f2f4804a498ceda2da939d05e8007277deecf5
dagray0524 <devone.gray@ubalt.edu>
2025-04-27 00:43:08 -0500

    Update - 04/27/2025
    Changed

--- Assets/Scripts/System/IDataPersistence.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDataPersistence 
-{ //Reference: https://m.youtube.com/watch?v=aUi9aijvpgs&pp=0gcJCdgAo7VqN5tD
-    void LoadData(GameData data);
-    void SaveData(ref GameData data);
-}

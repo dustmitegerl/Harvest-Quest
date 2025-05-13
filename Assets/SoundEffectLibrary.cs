08c77edf02a880391d7ce53bbc244e51cca1c6c7
dagray0524 <devone.gray@ubalt.edu>
2025-05-13 09:36:01 -0500

    In Development
    Adding SFX manager

---
+++ Assets/SoundEffectLibrary.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectLibrary : MonoBehaviour
+{
+    //referene to script: https://youtu.be/rAX_r0yBwzQ?si=Gc0FJe1N6o1QkeVZ
+    [SerializeField] private SoundEffectGroup[] soundEffectGroups;
+    private Dictionary<string, List<AudioClip>> soundDictionary;
+    
+    private void Awake()
+    {
+        InitializeDictionary();
+    }
+
+    private void InitializeDictionary()
+    {
+        soundDictionary = new Dictionary<string, List<AudioClip>>();
+        foreach(SoundEffectGroup soundEffectGroup in soundEffectGroups)
+        {
+            soundDictionary[soundEffectGroup.name] = soundEffectGroup.audioClips;
+        }
+    }
+  
+  public AudioClip GetRandomClip(string name)
+  {
+    if(soundDictionary.ContainsKey(name))
+    {
+        List<AudioClip> audioCLips = soundDictionary[name];
+        if(audioCLips.Count > 0)
+        {
+            return audioCLips[UnityEngine.Random.Range(0, audioCLips.Count)];
+        }
+    }
+    return null;
+  }
+}
+
+[System. Serializable]
+
+public struct SoundEffectGroup
+{
+    public string name;
+    public List<AudioClip> audioClips;
+}
